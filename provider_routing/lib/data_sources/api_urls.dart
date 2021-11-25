94baa1e9d52bf856e7e5c19a89a14c5622ef2954
BaoFlutter <baoflutter@gmail.com>
2021-11-25 22:05:04 +0700

    add provider

---
+++ provider_routing/lib/data_sources/api_urls.dart
@@ -0,0 +1,4 @@
+class ApiUrl {
+  static final VIDEO_URL = Uri.parse("https://api.zmax.vn/api/videos");
+
+}
---
+++ provider_routing/lib/views/video_list_screen.dart
@@ -0,0 +1,91 @@
+import 'package:api_lession/data_sources/api_services.dart';
+import 'package:api_lession/models/video_model.dart';
+import 'package:api_lession/providers/video_list_provider.dart';
+import 'package:api_lession/resources/strings.dart';
+import 'package:api_lession/views/home_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+
+class VideoListScreen extends StatefulWidget {
+  const VideoListScreen({Key? key}) : super(key: key);
+
+  @override
+  _VideoListScreenState createState() => _VideoListScreenState();
+}
+
+class _VideoListScreenState extends State<VideoListScreen> {
+  Future<List<VideoModel>>? futureVideoList;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadVideos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(VIDEO_LIST),
+        leading: IconButton(
+          icon: Icon(Icons.backspace_rounded),
+          onPressed: (){
+            //Navigator.pop(context);]
+            Get.back();
+            /*
+            var route = MaterialPageRoute(builder: (context) => HomeScreen());
+            Navigator.pushAndRemoveUntil(context, route, (route)=> false);
+
+             */
+          },
+        ),
+      ),
+      body: Container(
+        height: Get.height,
+        width: Get.width,
+        child: FutureBuilder<List<VideoModel>>(
+          future: futureVideoList,
+            builder: (context, snapshot)
+        {
+          if(snapshot.hasError)
+            return Center(
+             child: Text("Load dữ liệu lỗi"),
+          );
+          if(!snapshot.hasData) return
+            Center(
+            child: CircularProgressIndicator()
+          );
+          List<VideoModel> videoList = snapshot.data!;
+          context.read<VideoListProvider>().updateVideoList(videoList: videoList);
+          return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: videoList.length,
+              itemBuilder: (BuildContext context, int index) {
+                VideoModel videoModel = videoList[index];
+                return Card(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Image.network(videoModel.url_photo!),
+                      Text(videoModel.title!)
+                    ],
+                  )
+                );
+              }
+          );
+
+        }),
+      ),
+    );
+  }
+
+  loadVideos()
+  {
+    futureVideoList = ApiServices().fetchVideoList();
+  }
+
+}
